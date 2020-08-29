718bfa2276141f9ead6ed3cbed332c77be563f28
David O <davideoguche@gmail.com>
2020-08-29 13:45:34 +0100

    Added Notes Service

---
+++ lib/services/notes_service.dart
@@ -0,0 +1,39 @@
+
+import 'package:quick_notes/models/note_for_listing.dart';
+
+class NotesService {
+  List<NoteForListing> getNotesList() {
+    return [
+      new NoteForListing(
+          noteID: "1",
+          createDateTime: DateTime.now(),
+          latestEditDateTime: DateTime.now(),
+          noteTitle: "Note 1"
+      ),
+      new NoteForListing(
+          noteID: "2",
+          createDateTime: DateTime.now(),
+          latestEditDateTime: DateTime.now(),
+          noteTitle: "Note 2"
+      ),
+      new NoteForListing(
+          noteID: "3",
+          createDateTime: DateTime.now(),
+          latestEditDateTime: DateTime.now(),
+          noteTitle: "Note 3"
+      ),
+      new NoteForListing(
+          noteID: "4",
+          createDateTime: DateTime.now(),
+          latestEditDateTime: DateTime.now(),
+          noteTitle: "Note 4"
+      ),
+      new NoteForListing(
+          noteID: "5",
+          createDateTime: DateTime.now(),
+          latestEditDateTime: DateTime.now(),
+          noteTitle: "Note 5"
+      ),
+    ];
+  }
+}
